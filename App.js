1546a0e5cf6a68e75c6bfa8f111902e57eb38d73
kacn1z-del <kacn1z@gmail.com>
2026-06-29 21:09:32 -0600

    Actualizar App.js

--- App.js
+++ App.js
@@ -1,1 +1,27 @@
+{
+  "expo": {
+    "name": "Matriz Contable CR",
+    "slug": "matriz-contable-cr",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "splash": {
+      "backgroundColor": "#1e5208"
+    },
+    "assetBundlePatterns": ["**/*"],
+    "ios": {
+      "supportsTablet": true,
+      "bundleIdentifier": "com.kcn.matrizcontablecr"
+    },
+    "android": {
+      "package": "com.kcn.matrizcontablecr"
+    },
+    "extra": {
+      "eas": {
+        "projectId": "a2de97cd-ddbb-42c6-bd8a-97b0e27c7595"
+      }
+    }
+  }
+}
 
