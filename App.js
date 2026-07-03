959d6a389c4c48fe2a8cff5544e31fed24650d15
kacn1z-del <kacn1z@gmail.com>
2026-07-03 13:40:53 -0600

    Actualizar App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { StatusBar } from 'expo-status-bar'
 import { LangProvider, useLang } from './src/context/LangContext'
 import HomeScreen from './src/screens/HomeScreen'
+import FunctionsScreen from './src/screens/FunctionsScreen'
 import SheetEditor from './src/components/SheetEditor'
 import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native'
 
@@ -50,13 +51,10 @@ function AppNavigator() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Sheet" component={SheetScreen} />
+        <Stack.Screen name="Funciones" component={FunctionsScreen} />
         <Stack.Screen
           name="Factura"
           children={(props) => <PlaceholderScreen {...props} title="🧾 Factura Electrónica 4.3" />}
-        />
-        <Stack.Screen
-          name="Funciones"
-          children={(props) => <PlaceholderScreen {...props} title="fx Biblioteca de Funciones (452)" />}
         />
         <Stack.Screen
           name="Sibo"
@@ -85,4 +83,3 @@ const styles = StyleSheet.create({
   backBtn: { fontSize: 13, fontWeight: '700', color: '#2d7a0c' },
   toolbarTitle: { fontSize: 13, fontWeight: '700', color: '#3d6020' },
 })
-
